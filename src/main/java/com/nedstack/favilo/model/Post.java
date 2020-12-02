e07edf00ae0de142225b1fc674f33a7d5348f448
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-12-02 18:34:11 +0100

    post annotaions

--- src/main/java/com/nedstack/favilo/model/Post.java
+++ src/main/java/com/nedstack/favilo/model/Post.java
@@ -1,4 +1,43 @@
 package com.nedstack.favilo.model;
 
+
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.NotFound;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+    @NotBlank(message = "Post Name Cannot be blank")
+    private String postName;
+    @Nullable
+    private String url;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private  User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subredits subredits;
+
+
 }
