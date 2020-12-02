1c2b371d612be42f96b6cb8ba18f5a7a91334659
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-12-02 23:56:54 +0100

    password encoder

--- src/main/java/com/nedstack/favilo/service/AuthService.java
+++ src/main/java/com/nedstack/favilo/service/AuthService.java
@@ -3,6 +3,8 @@ package com.nedstack.favilo.service;
 
 import com.nedstack.favilo.dto.RegisterRequest;
 import com.nedstack.favilo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -10,11 +12,14 @@ import java.time.Instant;
 @Service
 public class AuthService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword());
         user.setCreated(Instant.now());
         user.setEnabled(false);
     }
