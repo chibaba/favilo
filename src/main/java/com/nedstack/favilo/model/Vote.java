1af1f593fc637a7f63cf3f9702b4aa324c0972c0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-12-02 20:01:02 +0100

    subredditt annotaions

--- src/main/java/com/nedstack/favilo/model/Vote.java
+++ src/main/java/com/nedstack/favilo/model/Vote.java
@@ -1,4 +1,30 @@
 package com.nedstack.favilo.model;
 
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.util.Lazy;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Builder
+
 public class Vote {
+    @Id
+    @GeneratedValue(strategy =  IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
 }
