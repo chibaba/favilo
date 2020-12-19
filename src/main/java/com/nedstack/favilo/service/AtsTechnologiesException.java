add738d2e69ce7eb24a2ef511a1358da1e290361
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-12-19 14:39:14 +0100

    wip

---
+++ src/main/java/com/nedstack/favilo/service/AtsTechnologiesException.java
@@ -0,0 +1,7 @@
+package com.nedstack.favilo.service;
+
+public class AtsTechnologiesException extends RuntimeException {
+    public AtsTechnologiesException(String exMessage) {
+        super(exMessage);
+    }
+}
